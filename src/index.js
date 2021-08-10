5384d37f9a0ce8f3a368928c72cb4df1eaa1b84f
Trung Bui <bvtrungboy@gmail.com>
2021-08-10 16:13:10 +0700

    up code mvc

--- src/index.js
+++ src/index.js
@@ -4,19 +4,17 @@ const exphbs  = require('express-handlebars');
 const path = require('path');
 const app = express();
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json())
 app.use(morgan('combined'));
 //app.engine('handlebars', exphbs());
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, "resource/views"));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+route(app);
 
 app.listen(3000);
