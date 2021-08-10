688c2afed197aaaab8dd742111f90b1f1a6ed43d
Trung Bui <bvtrungboy@gmail.com>
2021-08-10 17:04:52 +0700

    update file

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const route = express.Router();
+            const express = require('express');
+            const route = express.Router();
 
-const newsController = require('../app/controllers/NewsController');
+        const newsController = require('../app/controllers/NewsController');
 
-route.use('/', newsController.index);
+        route.use('/', newsController.index);
 
 module.exports = route;
